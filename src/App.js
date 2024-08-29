fe2d35676b4de83747e2d16d3e864319b84c8db4
Hyoyoung <gyddl123@gmail.com>
2024-08-29 23:53:53 +0900

    Animation효과를 keyframes를 import하고 styled-component쓰는 방식과 같은 방식으로 사용한다.
    animation : {변수}로 사용가능하며 해당 html 안의 element에 접근 가능. &: 혹은 :를 사용 하여 span:hover와 같은 기능으로 사용 가능

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Input = styled.input.attrs({ required: true })`
-  background-color: tomato;
+const rotateAnimation = keyframes`
+  0%{
+  transform:rotate(0deg);
+  border-radius : 0px;
+  } 50% {
+  transform:rotate(360deg);
+  border-radius : 100px;
+  }
+  100%{
+    transform:rotate(0deg);
+  border-radius : 0px;
+  }
 `;
 
-const Input_test = styled(Input).attrs((props) => ({
-  id: props.id,
-}))`
-  background-color: white;
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotateAnimation} 1s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 20px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
 
 function App() {
   return (
-    <Father>
-      <Input as="button" />
-      <Input_test id="300" />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>😃</span>
+      </Box>
+    </Wrapper>
   );
 }
 
