c4b93e45f4e7e6f99f23e7715494edc445e7c822
Hyoyoung <gyddl123@gmail.com>
2024-08-29 23:22:30 +0900

    style 뿐만아니라, attr도 props로 커스터마이징 가능함 const Input = styled.input.attrs((props) => ({ type: props.type, placeholder: props.ph || "write something", }))` background-color: tomato; `; ==> Input ph="hello" type="text" Input Input ph="hallo " type="number" Input ----------------------------------------------------------------- styled(Something) 으로 확장된 컴포넌트도 attr 값을 props 로 커스터마이징 가능함 const Circle = styled(Box).attrs((props) => ({ id: props.id }))` border-radius: 50%; `; ==> Circle id="300" blabla /Circle

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ const Input = styled.input.attrs({ required: true })`
   background-color: tomato;
 `;
 
+const Input_test = styled(Input).attrs((props) => ({
+  id: props.id,
+}))`
+  background-color: white;
+`;
+
 function App() {
   return (
     <Father>
       <Input as="button" />
-      <Input />
+      <Input_test id="300" />
       <Input />
       <Input />
       <Input />
