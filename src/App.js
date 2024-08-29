19019222901c613b8ea3a2c97fb104ebb3a0a8c6
Hyoyoung <gyddl123@gmail.com>
2024-08-29 23:09:29 +0900

    as을 사용하여 엘리먼트를 다른 엘리먼트로 교체할 수 있습니다. ex) Container as="header" https://styled-components.com/docs/basics#extending-styles

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Input = styled.input.attrs({required:true})`
+const Input = styled.input.attrs({ required: true })`
   background-color: tomato;
 `;
 
 function App() {
   return (
     <Father>
-      <Input />
+      <Input as="button" />
       <Input />
       <Input />
       <Input />
