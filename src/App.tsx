67310f04406e9171b39a24e5ed2ca598b35c5f54
Hyoyoung <gyddl123@gmail.com>
2024-08-31 18:15:46 +0900

    ```tsx const [value, setValue] = useState(0); setValue(2); // ok setValue("hello"); // ok ```
    - 기존의 state 변수는 `useState()` 메서드의 초기값 초기화가 발생하면, 해당 초기 값의 데이터 타입을 따라 간다.
    - 하지만
    - 특정 데이터 타입을 지정
    - 2개 이상의 데이터 타입을 지정
    
    하고 싶은 경우, `꺽쇠 괄호(< >)`를 통해서 데이터 타입 지정이 가능하다.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import styled, { keyframes } from "styled-components";
+import React from 'react';
+import Circle from "./Circle";
+
+
+function App() {
+  return (
+    <div>
+      <Circle borderColor="yellow" bgColor="teal"/>
+      <Circle text="I'm here" bgColor="tomato"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Circle.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import React, { useState } from 'react';
+
+interface ContainerProps{
+    bgColor : string;
+    borderColor : string;
+}
+
+const Container = styled.div<ContainerProps>`
+    width:200px;
+    height:200px;
+    background-color:  ${props => props.bgColor};
+    border-radius: 100px;
+    border : 1px solid ${props => props.borderColor};
+`;
+
+interface CircleProps {
+    bgColor : string;
+    borderColor? : string;
+    text? :string;
+}
+
+function Circle({bgColor, borderColor, text = "default text"} : CircleProps) {
+    const [value, setVlaue] = useState<number|string>(0);
+    setVlaue(2)
+    return <Container bgColor={bgColor} borderColor={borderColor ?? "white"}>{text}</Container>
+}
+
+// interface PlayerShpae{
+//     name : string,
+//     age : number
+// }
+
+// const sayHello = (playerObj:PlayerShpae) => `Hello ${playerObj.name} You are ${playerObj.age} years old`
+
+
+// sayHello({name : "nico", age : 29})
+// sayHello({name : "hi", age : 12, hello:1})
+
+export default Circle
