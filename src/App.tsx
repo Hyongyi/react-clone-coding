5d7afd9766c9008bbf8581fe8e06892b9685aef7
Hyoyoung <gyddl123@gmail.com>
2024-09-04 23:45:56 +0900

    react-router-dom 6.4 version upgrade

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,13 @@
-import styled from "styled-components";
-import React, { useState } from 'react';
+import React from 'react';
+import Router from './Router';
 
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`
-const H1 = styled.h1`
-    color:${(props) => props.theme.textColor};
-`
 
-interface DummyProps{
-    text:string;
-    active?:boolean;
-}
-
-function Dummy({text, active = true}:DummyProps) {
-    return <h1>{text}</h1>
-}
-
-function App() {
-    const onClick = (event:React.FormEvent<HTMLButtonElement>) =>{
-     
-    }
-  return (
-    <Container>
-        <Dummy active text="Hello" />
-        <form>
-        <button onClick={onClick}>click me</button>
-        </form>
-    </Container>
-  );
+function App(){
+    return (
+    <div>
+        <Router />
+    </div>
+    )
 }
 
 export default App;
