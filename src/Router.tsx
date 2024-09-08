83bb6e3c9fbd79800c30d9f0c3189e56260df65d
Hyoyoung <gyddl123@gmail.com>
2024-09-08 11:46:56 +0900

    Outlet
    Outlet은 하위 경로 요소를 렌더링하기 위해 상위 경로 요소에서 사용합니다.
    이렇게 하면 하위 경로가 렌더링될 때 중첩된 UI가 표시될 수 있습니다. 상위 경로가 정확히 일치하면 하위 index 경로를 렌더링하거나 index 경로가 없으면 아무것도 렌더링하지 않습니다.
    ex) < Outlet />
    
    https://reactrouter.com/en/main/components/outlet

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,6 +6,8 @@ import About from "./screens/About";
 import Root from "./Root"
 import NotFound from "./screens/NotFound";
 import ErrorComponent from "./components/ErrorComponent";
+import User from "./screens/users/User";
+import Followers from "./screens/users/Followers";
 
 
 const router = createBrowserRouter([
@@ -21,6 +23,17 @@ const router = createBrowserRouter([
             {
                 path:"about",
                 element:<About />,
+            },
+            {
+                path:"users/:userId",
+                element : <User />,
+                children : [
+                    {
+                        path : "followers",
+                        element : <Followers />
+
+                    }
+                ]
             }
         ],
         errorElement : <NotFound />
---
+++ src/screens/users/User.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link, Outlet, useParams } from 'react-router-dom';
+import { users } from '../../db';
+
+function User(){
+    const {userId} = useParams();
+    return (
+    <div>
+        <h1>Users with id {userId} is named: {users[Number(userId) -1].name}</h1>
+        <hr />
+        <Link to="followers">See Followers</Link>
+        <Outlet />
+    </div>
+    ) 
+}
+    export default User;
