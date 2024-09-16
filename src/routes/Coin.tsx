f292f0c15d4edecafad7108f1792ac2ecf2a5513
Hyoyoung <gyddl123@gmail.com>
2024-09-16 23:52:43 +0900

    5.3

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -3,7 +3,9 @@ import { useParams } from 'react-router-dom';
 
 function Coin() {
     const {coinId} = useParams<{coinId:string}>();
+    const id = 1 ;
     return <h1>Coin : {coinId}</h1>;
+
 }
 
 export default Coin;
